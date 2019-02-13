0efcb323a81b0d22b31399c85bfff19ed98ca25e
Matthew A. Romero <mzr0049@tigermail.auburn.edu>
2019-02-13 10:30:47 -0600

    Major changes that I did. I did not commit after every change, however. I apologize.

--- part_2/sys_info_Romero.sh
+++ part_2/sys_info_Romero.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-#Below are commands that give us the time and date, a list of user, and the system uptime
+#Below are commands that give us the time and date, a list of user, and the system uptime. We will add "\n" for clean line breaks
+#to make things easier to read.
 time=`date | awk '{print $2, $3, $4}'`
+printf "\n"
 
 #Now we will print to the screen
 printf "Here is the date and time - ${time} \n"
+
+#Let's see who is logged on the asc
+who
+printf "\n"
+printf "Too much info. Let's only display username. \n"
+printf "\n"
+
+#That looks messy. Let's clean it up with an awk command used with a pipe
+who | awk '{print $1}'
+
+#Now, let's look at the uptime.
+printf "\n"
+printf "Here's the uptime \n"
+uptime -p
